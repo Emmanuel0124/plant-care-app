f41e70187f1bbda05d4398ffcdd7842e8ef11082
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-04 10:05:45 -0500

    add create action for plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,5 +3,14 @@ class PlantsController < ApplicationController
     @plants = Plant.all
     render :index
   end
+  def create
+    @plant = Plant.create(
+      name: params[:name],
+      description: params[:description],
+      amount_of_sun: params[:amount_of_sun],
+      days_to_water: params[:days_to_water],
+    )
+    render :show
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   get "/plants" => "plants#index"
+  post "/plants" => "plants#create"
 
 end
